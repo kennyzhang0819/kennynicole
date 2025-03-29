70fda1aaeeae542b2feb621fd7ebe189eec32979
Kenny Zhang <122967787+kennyzhang0819@users.noreply.github.com>
2025-03-29 15:12:06 -0700

    alow domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  images: {
+    domains: [
+      'm.media-amazon.com',
+      'www.omdbapi.com',
+    ],
+  },
 };
 
 export default nextConfig;
